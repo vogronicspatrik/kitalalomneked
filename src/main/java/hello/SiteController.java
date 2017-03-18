761ba0c40c3f363782e45bf7078f58617f774487
vogronicspatrik <vogronicspatrik@gmail.com>
2017-03-18 17:05:28 +0100

    gif fckd

---
+++ src/main/java/hello/SiteController.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class SiteController {
+
+    @RequestMapping("/")
+    public String greeting(){
+        return "greeting";
+    }
+}
